9ce4ead1aab2e40b982a4cb5d287586e95d5c6cf
ea.kichaev <kichaev1@mail.ru>
2022-05-17 14:52:35 +0300

    fic method name of SIGN_IN

--- src/graphql/queries/auth.ts
+++ src/graphql/queries/auth.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const SIGN_IN = gql`
   query login($signInDto: SignInDto!) {
-    signIn(signInDto: $signInDto) {
+    login(signInDto: $signInDto) {
       accessToken
     }
   }
